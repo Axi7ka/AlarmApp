4f491c0152f1b33ae08f5acdff7c256e90be6a91
Axi7ka <alexandra_ch_2010@yahoo.com>
2022-10-21 19:32:53 +0300

    Resolved bug

--- app/src/main/java/com/axichise/alarmapp/MainActivity.kt
+++ app/src/main/java/com/axichise/alarmapp/MainActivity.kt
@@ -16,13 +16,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.btn)
         val editText = findViewById<EditText>(R.id.tv2)
-        button.setOnClickListener{
-            var sec: Int = editText.text.toString().toInt()
-            var i = Intent(applicationContext, MyBroadcastReciver::class.java)
-            var pi: PendingIntent = PendingIntent.getBroadcast(applicationContext,11,i,PendingIntent.FLAG_IMMUTABLE)
-            var am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() + (sec*1000),pi)
-            Toast.makeText(applicationContext, "Alarm is set for $sec seconds", Toast.LENGTH_LONG).show()
+        fun isNumber(sec: String?): Boolean{
+            return  if(sec.isNullOrBlank()) false
+            else  sec.all{
+                Character.isDigit(it)
+            }
         }
+        button.setOnClickListener{
+            var sec = editText.text.toString()
+            var i = Intent(applicationContext, MyBroadcastReciver::class.java)
+            if(isNumber(sec) ) {
+                var sec:Int = editText.text.toString().toInt()
+                var pi: PendingIntent = PendingIntent.getBroadcast(applicationContext,11,i,PendingIntent.FLAG_IMMUTABLE)
+                var am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (sec * 1000), pi)
+                Toast.makeText(
+                    applicationContext,
+                    "Alarm is set for $sec seconds",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            else
+                Toast.makeText(applicationContext, "Error! Not a valid number",Toast.LENGTH_SHORT).show()
+            }
     }
 }
